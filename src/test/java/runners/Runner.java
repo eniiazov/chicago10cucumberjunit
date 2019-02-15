bcde6d458d6762fcf88388f161cdc99cc8293c99
Cybertek Chicago <esen@cybertekschool.com>
2019-02-14 19:37:48 -0600

    Cucumber with jUnit, API Rest Assured, POJO

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,12 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber",
+        plugin = {"html:target/cucumber", "json:target/report.json"},
         features = "src/test/resources/features",
         glue = "steps",
         tags = "@studentTitle",
         dryRun = false
 )
 public class Runner {
-
+    
 }
